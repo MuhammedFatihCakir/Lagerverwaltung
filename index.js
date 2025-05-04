a253c69412fe569cd104f347953b325b028ff8b4
MuhammedFatihCakir <muhammed.cakir@dci-student.org.com>
2025-05-04 18:10:34 +0200

    Arbeitsschritte definiert

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 import fs from 'fs/promises';
 import inquirer from 'inquirer';
+import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import { fileURLToPath } from 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const inventoryFile = `${__dirname}/inventory.json`;
+
+// 1. inventory.json laden wenn nicht gegeben dann leeres array setzen
+// 2. von den änderungen der hinzufügungen nehmen und überschreiben
+// 3. Produkt hinzufuegen
+// 4. Produkte listen
+// 5. Produkt anzeigen
+// 6. Produkt bearbeiten
+// 7. Produkt löschen
+// 8. mainmenu erstellen
